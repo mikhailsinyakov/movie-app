cbbcaa4e3de85441e0345f81c6879749b99dd1a2
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2020-03-04 19:31:51 +0300

    Add cancel feature to movieAPI

--- src/api/movieAPI/index.js
+++ src/api/movieAPI/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CancelToken } from "axios";
 import getConfigPromise from "./getConfigPromise";
 
 const request = axios.create({
@@ -13,46 +13,61 @@ const getRegion = lang => (lang === "en" ? "US" : "RU");
 const getPosterSrc = (config, posterPath) =>
   `${config.imageBaseUrl}${config.posterSize}${posterPath}`;
 
-export const getMoviesList = async ({ language = "en", page = 1 } = {}) => {
-  const config = await getConfigPromise;
-  const path = "/movie/now_playing";
-  const response = await request({
-    url: path,
-    params: {
-      language,
-      region: getRegion(language),
-      page
-    }
-  });
+export const getMoviesList = ({ language = "en", page = 1 } = {}) => {
+  const source = CancelToken.source();
 
-  return {
-    ...response.data,
-    results: response.data.results.map(movie => ({
-      ...movie,
-      genres: movie.genre_ids.map(genreId => config.genres[language][genreId]),
-      poster_src: movie.poster_path && getPosterSrc(config, movie.poster_path)
-    }))
-  };
+  const promise = (async () => {
+    const config = await getConfigPromise;
+    const path = "/movie/now_playing";
+    const response = await request({
+      url: path,
+      params: {
+        language,
+        region: getRegion(language),
+        page
+      },
+      cancelToken: source.token
+    });
+
+    return {
+      ...response.data,
+      results: response.data.results.map(movie => ({
+        ...movie,
+        genres: movie.genre_ids.map(
+          genreId => config.genres[language][genreId]
+        ),
+        poster_src: movie.poster_path && getPosterSrc(config, movie.poster_path)
+      }))
+    };
+  })();
+
+  return { cancel: source.cancel, promise };
 };
 
-export const getMovieDetails = async ({ language, movieId }) => {
-  const config = await getConfigPromise;
-  const path = `/movie/${movieId}`;
-  const response = await request({
-    url: path,
-    params: {
-      language,
-      region: getRegion(language)
-    }
-  });
-  const movieData = response.data;
-  return {
-    ...movieData,
-    genres: movieData.genres.map(genre => genre.name),
-    poster_src:
-      movieData.poster_path && getPosterSrc(config, movieData.poster_path),
-    production_countries: movieData.production_countries.map(
-      contry => contry.name
-    )
-  };
+export const getMovieDetails = ({ language, movieId }) => {
+  const source = CancelToken.source();
+
+  const promise = (async () => {
+    const config = await getConfigPromise;
+    const path = `/movie/${movieId}`;
+    const response = await request({
+      url: path,
+      params: {
+        language,
+        region: getRegion(language)
+      }
+    });
+    const movieData = response.data;
+    return {
+      ...movieData,
+      genres: movieData.genres.map(genre => genre.name),
+      poster_src:
+        movieData.poster_path && getPosterSrc(config, movieData.poster_path),
+      production_countries: movieData.production_countries.map(
+        contry => contry.name
+      )
+    };
+  })();
+
+  return { cancel: source.cancel, promise };
 };
