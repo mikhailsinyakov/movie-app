fe5ed75ec67698be3cb1e89ad45c63ad258a9036
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2020-03-11 12:44:32 +0300

    Move fetch logic from App to MoviesList

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,13 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { getMoviesList } from "api/movieAPI";
-import { withLanguageContext } from "./Language";
 import HomeScreen from "screens/Home";
 import MovieScreen from "screens/Movie";
 
-const moviesDataCache = {};
-
-const App = ({ language }) => {
-  const [loading, setLoading] = useState(false);
-  const initialMoviesData = {
-    page: 0,
-    total_pages: 0,
-    results: []
-  };
-  const [moviesData, setMoviesData] = useState(initialMoviesData);
-
-  const fetchMoviesData = (page = 1) => {
-    if (page === 1 && moviesDataCache[language]) {
-      setMoviesData(moviesDataCache[language]);
-    } else {
-      setLoading(true);
-      const { cancel, promise } = getMoviesList({ language, page });
-      (async () => {
-        try {
-          const newMoviesData = await promise;
-          const moviesDataToSave = {
-            ...newMoviesData,
-            results: [...moviesData.results, ...newMoviesData.results]
-          };
-          moviesDataCache[language] = moviesDataToSave;
-          setMoviesData(moviesDataToSave);
-          setLoading(false);
-        } catch (e) {
-          console.error(e);
-        }
-      })();
-      return () => cancel("Component has unmounted");
-    }
-  };
-
-  useEffect(fetchMoviesData, []);
-
-  useEffect(() => {
-    setMoviesData(initialMoviesData);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [language]);
-
-  useEffect(() => {
-    if (!moviesData.results.length) {
-      return fetchMoviesData();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [moviesData]);
-
+const App = () => {
   return (
     <Router>
       <Route exact path="/">
-        <HomeScreen
-          moviesData={{
-            list: moviesData.results,
-            loading,
-            completed: moviesData.page === moviesData.total_pages
-          }}
-          getMoreMovies={() => fetchMoviesData(moviesData.page + 1)}
-        />
+        <HomeScreen />
       </Route>
       <Route path="/movie/:id">
         <MovieScreen />
@@ -74,8 +16,4 @@ const App = ({ language }) => {
   );
 };
 
-App.propTypes = {
-  language: PropTypes.string.isRequired
-};
-
-export default withLanguageContext(App);
+export default App;
--- src/components/MoviesList/index.js
+++ src/components/MoviesList/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { withLanguageContext } from "../Language";
 import MovieOverview from "./MovieOverview";
 import Button from "shared/Button";
-import { isNull } from "helpers/customPropCheckers";
+import { getMoviesList } from "api/movieAPI";
+import cache from "cache";
 
 const Loading = styled.div`
   text-align: center;
@@ -20,19 +21,64 @@ const Wrapper = styled.div`
   margin: 0 auto;
 `;
 
-const MoviesList = ({ getUIText, moviesData, getMoreMovies, className }) => {
-  const { list, loading, completed } = moviesData;
+const MoviesList = ({ language, getUIText, className }) => {
+  const [loading, setLoading] = useState(false);
+  const initialMoviesData = {
+    page: 0,
+    total_pages: 0,
+    results: []
+  };
+  const [moviesData, setMoviesData] = useState(initialMoviesData);
+
+  const fetchMoviesData = (page = 1) => {
+    if (page === 1 && cache.has("movies", "latest", language)) {
+      setMoviesData(cache.get("movies", "latest", language));
+    } else {
+      setLoading(true);
+      const { cancel, promise } = getMoviesList({ language, page });
+      (async () => {
+        try {
+          const newMoviesData = await promise;
+          const moviesDataToSave = {
+            ...newMoviesData,
+            results: [...moviesData.results, ...newMoviesData.results]
+          };
+          cache.set("movies", "latest", language, moviesDataToSave);
+          setMoviesData(moviesDataToSave);
+          setLoading(false);
+        } catch (e) {
+          console.error(e);
+        }
+      })();
+      return () => cancel("Component has unmounted");
+    }
+  };
+  const getMoreMovies = () => fetchMoviesData(moviesData.page + 1);
+
+  useEffect(fetchMoviesData, []);
+
+  useEffect(() => {
+    setMoviesData(initialMoviesData);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [language]);
+
+  useEffect(() => {
+    if (!moviesData.results.length) {
+      return fetchMoviesData();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [moviesData]);
 
   return (
     <Wrapper>
       <div className={className}>
-        {list.map(movie => (
+        {moviesData.results.map(movie => (
           <MovieOverview key={movie.id} movie={movie} />
         ))}
       </div>
       {loading ? (
         <Loading>{getUIText("loading")}...</Loading>
-      ) : !completed ? (
+      ) : moviesData.page !== moviesData.total_pages ? (
         <Button onClick={getMoreMovies}>{getUIText("more")}</Button>
       ) : null}
     </Wrapper>
@@ -40,21 +86,8 @@ const MoviesList = ({ getUIText, moviesData, getMoreMovies, className }) => {
 };
 
 MoviesList.propTypes = {
-  getUIText: PropTypes.func.isRequired,
-  moviesData: PropTypes.shape({
-    list: PropTypes.arrayOf(
-      PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        id: PropTypes.number.isRequired,
-        poster_src: PropTypes.oneOfType([PropTypes.string, isNull]),
-        vote_average: PropTypes.number,
-        genres: PropTypes.arrayOf(PropTypes.string)
-      })
-    ).isRequired,
-    loading: PropTypes.bool.isRequired,
-    completed: PropTypes.bool.isRequired
-  }).isRequired,
-  getMoreMovies: PropTypes.func.isRequired
+  language: PropTypes.string.isRequired,
+  getUIText: PropTypes.func.isRequired
 };
 
 const StyledMoviesList = styled(MoviesList)`
